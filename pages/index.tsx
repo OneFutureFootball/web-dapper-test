67a089f1a4f730d58c3762fb6700ba17f4197fc8
Shayan S <shayan@onefuturefootball.com>
2023-02-07 10:32:04 +0800

    Working dapper wallet integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,43 +2,13 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import * as fcl from "@onflow/fcl";
-import { configFCL, buyTx } from '../flow/config';
 import { useState, useEffect } from 'react';
 
+import { configFCL } from '../flow/config';
+import { executeList, executeInitialiseAccount, executeDirectBuy } from '../flow/txFunctions';
+
 configFCL();
-//merchantAccountAddress, storefrontAddress, listingResourceID, expectedPrice
-// testnet addr, testnet addr, nft id, expected price
 
-
-
-const sendTransaction = async () => {
-  // This FCL code snippet showcases FCL code interaction with the Dapper Wallet
-  const dapperAuthz = fcl.authz;
-
-  const merchantAddress = "0xa1d82983873b0a61"
-  const storefrontAddress = merchantAddress
-  const listingResourceID = 0;
-  const expectedPrice = Number(9.99).toFixed(8);
-
-
-  const tx = await fcl.send([
-    fcl.transaction(buyTx),
-    fcl.payer(fcl.authz),
-    fcl.proposer(fcl.authz),
-    fcl.authorizations([dapperAuthz, fcl.authz]),
-    fcl.args([
-      fcl.arg(merchantAddress, fcl.t.Address),
-      fcl.arg(storefrontAddress, fcl.t.Address),
-      fcl.arg(listingResourceID, fcl.t.UInt64),
-      fcl.arg(expectedPrice, fcl.t.UFix64),
-    ]),
-    fcl.limit(1000),
-  ]).then((tx: any) => {
-    console.log(tx);
-    console.log(fcl.decode(tx));
-    return fcl.decode(tx);
-  });
-}
 
 const Home: NextPage = () => {
 	const [user, setUser] = useState<fcl.User>({ loggedIn: null });
@@ -62,12 +32,18 @@ const Home: NextPage = () => {
           <button className={styles.card} onClick={fcl.unauthenticate}>
             Disconnect wallet
           </button>
-          <button className={styles.card} onClick={sendTransaction}>
-            Test transaction
-          </button>
           {user.addr}
-
-
+        </div>
+        <div className={styles.grid}>
+        <button className={styles.card} onClick={executeInitialiseAccount}>
+            Account initialisation transaction
+          </button>
+          <button className={styles.card} onClick={executeList}>
+            Listing transaction
+          </button>
+          <button className={styles.card} onClick={executeDirectBuy}>
+            Direct purchase transaction 
+          </button>
         </div>
       </main>
     </div>
